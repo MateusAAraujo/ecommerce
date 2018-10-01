74d36bde08884f042b6a42a46eff87b57b07572b
Mateus Alberto <mateus@mateus-C-05>
2018-10-01 18:01:01 -0300

    Recuperar Senha.

--- index.php
+++ index.php
@@ -145,5 +145,78 @@ $app->post('/admin/users/:iduser', function($iduser){
     exit;
 });
 
+//Rota para recuperar senha
+$app->get('/admin/forgot', function() {
+    
+    $page = new PageAdmin([
+	'header'=>false,
+	'footer'=>false
+    ]);
+    
+    $page->setTpl('forgot');
+});
+
+//Rota para checar email e validar email
+$app->post('/admin/forgot', function() {
+    
+    $user = User::getForgot($_POST['email']);
+    
+    header('Location: /admin/forgot/sent');
+    exit;
+});
+
+//Rota envio email recuperação de senha
+$app->get('/admin/forgot/sent', function() {
+    
+    $page = new PageAdmin([
+	'header'=>false,
+	'footer'=>false
+    ]);
+    
+    $page->setTpl('forgot-sent');
+});
+
+//Rota para resetar senha antiga e gerar nova senha
+$app->get('/admin/forgot/reset', function() {
+    
+    $user = User::validForgotDecrypt($_GET['code']);
+    
+    $page = new PageAdmin([
+	'header'=>false,
+	'footer'=>false
+    ]);
+    
+    $page->setTpl('forgot-reset', array(
+	'name'=>$user['desperson'],
+	'code'=>$_GET['code']
+    ));
+});
+
+//Rota para validar s setar nova senha
+$app->post('/admin/forgot/reset', function() {
+    
+    $forgot = User::validForgotDecrypt($_POST['code']);
+    
+    User::setForgotUsed($forgot['idrecovery']);
+    
+    $user = new User();
+    
+    $user->get((int)$forgot['iduser']);
+    
+    //Encriptografa a nova senha
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT, ['cost'=>12]);
+    
+    //Passa nova senha (hash) criptografada para salvar no bd
+    $user->setPassword($password);
+    
+    $page = new PageAdmin([
+	'header'=>false,
+	'footer'=>false
+    ]);
+    
+    $page->setTpl('forgot-reset-success');
+    
+});
+
 $app->run();
 ?>
