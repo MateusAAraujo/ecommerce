0617339091fceb5575c65b31fdd2657139badc03
Mateus Alberto <mateus@mateus-C-05>
2018-10-19 18:01:02 -0300

    Integração PayPal.

--- site.php
+++ site.php
@@ -248,7 +248,17 @@ $app->post('/checkout', function(){
     
     $order->save();
     
-    header('Location: /order/'.$order->getidorder().'/pagseguro');
+    switch ((int)$_POST['payment-method']) {
+	
+	case 1:
+	    header('Location: /order/'.$order->getidorder().'/pagseguro');
+	    break;
+
+	case 2:
+	    header('Location: /order/'.$order->getidorder().'/paypal');
+	    break;
+    }
+    
     exit();
 });
 
@@ -275,6 +285,28 @@ $app->get('/order/:idorder/pagseguro', function($idorder){
 	    'areaCode'=>substr($order->getnrphone(), 0, 2),
 	    'number'=>substr($order->getphone(), 2, strlen($order->getnrphone()))
 	]
+    ]);
+});
+
+$app->get('/order/:idorder/paypal', function($idorder){
+    
+    User::verifyLogin();
+    
+    $order = new Order();
+    
+    $order->get((int)$idorder);
+    
+    $cart = $order->getCart();
+        
+    $page = new Page([
+	'header'=>false,
+	'footer'=>false
+    ]);
+    
+    $page->setTpl('payment-paypal', [
+	'order'=>$order->getValues(),
+	'cart'=>$cart->gerValues(),
+	'products'=>$cart->getValues()	
     ]);
 });
 
